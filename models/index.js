bb17bb5d3e25094aecf58415e313e35f0ea5c3c6
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-08-10 19:43:06 -0700

    Delete models directory

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./User');
-const Mood = require('./Mood');
-const Post = require('./Post');
-const Profile = require('./Profile');
-
-
-
-
-module.exports = { User ,Mood, Post, Profile };
