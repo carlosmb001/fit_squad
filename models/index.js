4be585f935fe6a3f145075539fae579e6c16fa19
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-08-10 19:46:13 -0700

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+const Mood = require('./Mood');
+const Post = require('./Post');
+const Profile = require('./Profile');
+
+
+
+
+module.exports = { User ,Mood, Post, Profile};
