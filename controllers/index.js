f59323ce4448a5e79dbc52fc89b537609eb991e1
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-08-15 14:15:55 -0700

    handlebar and auth route changs

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const authRoutes = require('./authRoutes');
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/user', authRoutes);
 
 module.exports = router;
