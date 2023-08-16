24552f55001ce036ec3edb00564c9963e01dd0a4
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-08-15 21:29:13 -0700

    created dashboard handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
-const authRoutes = require('./authRoutes');
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const homeRoutes = require("./homeRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
+const authRoutes = require("./authRoutes");
+const apiRoutes = require("./api");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/user', authRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/user", authRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
