9c0a43864af13b613ae374b58fb97c13b5801060
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-08-17 18:44:47 -0700

    Auth and route changes

--- controllers/historyRoutes.js
+++ controllers/historyRoutes.js
@@ -1,10 +1,49 @@
 const router = require("express").Router();
 const { logAuth } = require('../utils/auth')
+const { Workout,Goal, Post } = require('../models')
 
-router.get("/", logAuth, (req, res) => {
-  res.render("history", {
-    logged_in: req.session.logged_in,
-  });
+router.get("/", logAuth, async (req, res) => {
+    try {
+        const goals = await Goal.findAll({
+            /*   where: {
+                 id: req.params.id,
+             },*/
+             include: [{ model: Workout}],
+         });
+     
+         const plainGoals = goals.map((goal) => {
+           return goal.get({ plain: true });
+         });
+     
+         const posts = await Post.findAll({
+           /*   where: {
+                id: req.params.id,
+            },*/
+            include: [{ model: Workout}, {model:Goal}],
+        });
+     
+        const plainPosts = posts.map((post) => {
+          return post.get({ plain: true });
+        });
+     
+     
+         console.log( {
+           goals:plainGoals,
+           posts:plainPosts,
+           logged_in: req.session.logged_in,
+         })
+
+         res.render("history", {
+            goals:plainGoals,
+            posts:plainPosts,
+            logged_in: req.session.logged_in,
+          });
+
+    } catch (e) {
+    console.error(e);
+    res.status(500).end();
+  }
+
 });
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,21 +1,45 @@
-const profileData = {
-    name: "Clark Kent",
-    age: 32,
-    location: "Smallville",
-    mood: "Happy"
-  };
-  
-  function generateProfileCard(data) {
-    return `
-      <div class="profile-card">
-        <h2>${data.name}</h2>
-        <p>Age: ${data.age}</p>
-        <p>Location: ${data.location}</p>
-        <p>Mood: ${data.mood}</p>
-      </div>
-    `;
-  }
-  
-  const container = document.getElementById("profile-container");
+document.getElementById("submitButton").addEventListener("click", async (e) => {
+  e.preventDefault();
 
-  container.innerHTML = generateProfileCard(profileData);
+  const firstName = document.getElementById('firstNameInput').value;
+  const lastName = document.getElementById('lastNameInput').value;
+  const email = document.getElementById('emailInput').value;
+  const password = document.getElementById('passwordInput').value;
+
+  // Check if email is valid before proceeding
+  if (!isValidEmail(email)) {
+      console.error('Invalid email address');
+      return;
+  }
+
+  const userData = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      password: password
+  };
+
+  try {
+      const response = await fetch('/api/user', {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+          throw new Error('create user request failed');
+      }
+
+      window.location = '/profile'; // Redirect to a different page after successful submission
+  } catch (error) {
+      console.error('Error creating user:', error);
+  }
+});
+
+function isValidEmail(email) {
+  // Regular expression pattern for basic email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
