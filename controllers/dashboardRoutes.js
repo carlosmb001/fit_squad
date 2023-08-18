7211f8ce8fec2469099620e3fe4755452e736778
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-08-17 18:48:50 -0700

    T H I N G I E S

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const { logAuth } = require('../utils/auth')
+const { User, Workout } = require("../models");
 
-router.get("/", logAuth, (req, res) => {
+router.get("/", logAuth, async (req, res) => {
+  const userData = await User.findByPk(req.session.user_id);
+  
   res.render("dashboard", {
     logged_in: req.session.logged_in,
   });
