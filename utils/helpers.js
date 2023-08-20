38a52fcaf066b8bb4368a8442384544b3d74d814
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-08-19 23:36:12 -0700

    Changes for emoji and goal achieved flag

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,31 @@
 module.exports = {
-    format_date: date => {
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  format_date: (date) => {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  },
+  getMoodEmoji: (arg1) =>{
+    let emoji = "";
+    switch (arg1) {
+      case "Very Happy":
+        emoji = "😀";
+        break;
+      case "Happy":
+        emoji = "😀";
+        break;
+      case "Neutral":
+        emoji = "😐";
+        break;
+      case "Not Happy":
+        emoji = "🙁";
+        break;
+      case "Exhausted":
+        emoji = "😀";
+        break;
+      default:
+        emoji = "😤";
+        break;
     }
+    return emoji
+  },
 };
+
+
